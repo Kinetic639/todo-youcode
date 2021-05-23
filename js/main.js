99a567a7385d91e4ff4268a28311f1558e083d64
Kinetic639 <michal.stepien36@gmail.com>
2021-05-23 10:50:16 +0200

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
 {
   const tasks = [
     {
-      content: 'Iść na zakupy',
-      done: true
+      content: "Iść na zakupy",
+      done: true,
     },
     {
-      content: 'skończyc moduł 6',
-      done: false
+      content: "skończyc moduł 6",
+      done: false,
     },
-  ]
+  ];
 
   const render = () => {
     let htmlString = "";
-    for(const task of tasks) {
+    for (const task of tasks) {
       htmlString += `
       <li>
       ${task.content}
       </li>
-      `
+      `;
     }
-    document.querySelector(".js-tasks").innerHTML = htmlString
-  }
-  
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+  };
 
   const init = () => {
-    render()
-  }
-  init()
+    render();
+  };
+  init();
 }
